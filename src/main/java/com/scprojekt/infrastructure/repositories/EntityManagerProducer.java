518b1f06ab8f9e1c65f53eb23427275acf0ee014
Claudius Pomorska <claudius.pomorska@scprojekt.com>
2022-04-24 01:57:59 +0200

    Added missing files

--- src/main/java/com/scprojekt/infrastructure/repositories/EntityManagerProducer.java
+++ src/main/java/com/scprojekt/infrastructure/repositories/EntityManagerProducer.java
@@ -1,2 +1,26 @@
-package com.scprojekt.infrastructure.repositories;public class EntityManagerProducer {
+package com.scprojekt.infrastructure.repositories;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.enterprise.inject.Disposes;
+import jakarta.enterprise.inject.Produces;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Persistence;
+
+
+@ApplicationScoped
+public class EntityManagerProducer {
+
+    @Produces
+    @RequestScoped
+    public EntityManager createEntityManager(){
+        return Persistence.createEntityManagerFactory("infra").createEntityManager();
+    }
+
+    public void close(
+            @Disposes EntityManager entityManager) {
+        entityManager.close();
+    }
+
 }
+
--- src/test/java/com/scprojekt/infrastructure/repositories/test/TestEntityManagerProducer.java
+++ src/test/java/com/scprojekt/infrastructure/repositories/test/TestEntityManagerProducer.java
@@ -1,2 +1,25 @@
-package com.scprojekt.infrastructure.repositories.test;public class TestEntityManagerProducer {
+package com.scprojekt.infrastructure.repositories.test;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.enterprise.inject.Alternative;
+import jakarta.enterprise.inject.Disposes;
+import jakarta.enterprise.inject.Produces;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Persistence;
+
+@Alternative
+@ApplicationScoped
+public class TestEntityManagerProducer {
+
+    @Produces
+    @RequestScoped
+    protected EntityManager provideTestEntityManager() {
+        return Persistence.createEntityManagerFactory("infra").createEntityManager();
+    }
+
+    public void close(
+            @Disposes EntityManager entityManager) {
+        entityManager.close();
+    }
 }
